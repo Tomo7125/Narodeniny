9c6941f88b93406f00cf0f39567971a4820470b2
Tomo7125 <Tomashrdy3@gmail.com>
2023-09-17 21:00:59 +0200

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module sk.tomashrdy.narodeniny {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens sk.tomashrdy.narodeniny to javafx.fxml;
+    exports sk.tomashrdy.narodeniny;
+}
---
+++ src/main/java/sk/tomashrdy/narodeniny/HelloController.java
@@ -0,0 +1,8 @@
+package sk.tomashrdy.narodeniny;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+}
